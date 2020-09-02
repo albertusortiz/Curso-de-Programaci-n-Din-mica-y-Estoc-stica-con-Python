1b65a91d109ba9c26220f5c8e4c01717c1066926
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2020-09-02 10:47:28 -0500

    Creando algoritmo para el camino del borracho

---
+++ camino_aleatorio.py
@@ -0,0 +1,35 @@
+
+from borracho import BorrachoTradicional
+from campo import Campo
+from coordenada import Coordenada
+
+def caminata(campo, borracho, pasos):
+    inicio = campo.obtener_coordenada(borracho)
+
+    for _ in rango(pasos):
+        campo.mover_borracho(borracho)
+
+        return inicio.distancia(campo.obtener_coordenada(borracho))
+
+def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
+    borracho = tipo_de_borracho(nombre='David')
+    origen = Coordenada(0, 0)
+    distancias = []
+
+    for _ in range(numero_de_intentos):
+        campo = Campo()
+        campo.anadir_borracho(borracho, origen)
+        simulacion_caminata = caminata(campo, borracho, pasos)
+        distancias.append(round(simulacion_caminata, 1))
+
+def main(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
+    
+    for pasos in distancias_de_caminata:
+    distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
+
+
+if __name__ == '__main__':
+    distancias_de_caminata = [10, 100, 1000, 10000]
+    numero_de_intentos = 100
+
+    main(distancias_de_caminata, numero_de_intentos, BorrachoTradicional)
